f770f4bb3d9b4db7b607639e8db8df78c5f516b7
Thiago Silva <euthiagosrodrigues@gmail.com>
2022-06-14 10:37:13 -0300

    update

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,6 @@
 let menuMobile= document.getElementById("menu-mobile");
 let buttonMobile = document.getElementById("button-mobile");
-let ceepimg = document.querySelector(".section-one-ceep-img");
+let ceepimg = document.querySelector(".mySlides");
 let voltar = document.querySelector("#button-voltar");
 
 let stats = false; 
@@ -25,6 +25,31 @@ function atualizarImg() {
         buttonMobile.src = "./img/botaomenu.png";
     };
 };
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+    var x = document.getElementsByClassName("mySlides");
+
+    if (n > x.length) {
+        slideIndex = 1
+    };
+
+    if (n < 1) {
+        slideIndex = x.length
+    };
+
+    for (let i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    };
+
+    x[slideIndex-1].style.display = "block";
+}
 
 buttonMobile.addEventListener("click", abrirMenu);
 buttonMobile.addEventListener("click", atualizarImg);
